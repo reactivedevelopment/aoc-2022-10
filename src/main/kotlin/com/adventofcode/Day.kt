1b38f0db3080fe90190a6364a438626e52f6aaee
Daniel Demidko <demidko@farpost.com>
2022-12-10 10:18:20 +0500

    part one done

--- src/main/kotlin/com/adventofcode/Day.kt
+++ src/main/kotlin/com/adventofcode/Day.kt
@@ -1,14 +1,38 @@
 package com.adventofcode
 
-import com.adventofcode.Day.process
-import com.adventofcode.Day.solution
+import com.adventofcode.Processor.signalsSum
 
-object Day {
 
-  var solution = 0L; private set
+object Processor {
+  private var cycleCounter = 0L
+  private var xRegister = 1L
+  private var signalCycles = setOf(20L, 60L, 100L, 140L, 180L, 220L)
+  var signalsSum = 0L; private set
 
-  fun process(line: String) {
-    
+  private fun cycle() {
+    cycleCounter += 1
+    if(cycleCounter in signalCycles) {
+      signalsSum += (cycleCounter * xRegister)
+    }
+  }
+
+  private fun addX(v: Long) {
+    cycle()
+    cycle()
+    xRegister += v
+  }
+
+  private fun noop() {
+    cycle()
+  }
+
+  fun process(command: String) {
+    if(command == "noop") {
+      noop()
+      return
+    }
+    val value = command.removePrefix("addx ").toLong()
+    addX(value)
   }
 }
 
@@ -16,6 +40,6 @@ fun main() {
   ::main.javaClass
     .getResourceAsStream("/input")!!
     .bufferedReader()
-    .forEachLine(::process)
-  println(solution)
+    .forEachLine(Processor::process)
+  println(signalsSum)
 }
